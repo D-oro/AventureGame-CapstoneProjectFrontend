84dcd73c2672b317c7dd2ce075f6b1c6bbedc215
John MacLean <tribute2johnny@gmail.com>
2022-10-11 13:42:14 +0100

    player display test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import Request from './helpers/request';
+
+
+
 
 function App() {
+   const [players, setPlayers] = useState([]);
+
+   useEffect(() => {
+
+    const request = new Request()
+    request.get("/api/players")
+    .then((data) => {
+      console.log(data)
+      setPlayers(data);
+    })   
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
-  );
+  )
+  
 }
 
 export default App;
