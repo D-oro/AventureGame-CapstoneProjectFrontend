d918740549adcee9739d7d47815d83c5742d0028
D-oro <107217252+D-oro@users.noreply.github.com>
2022-10-15 14:44:29 +0100

    potion buttons show up in inventory, not yet rendering conditionally

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,16 +1,16 @@
+import '../style/Room/Inventory.css'
+
 const Inventory = () =>{
 
     return (
         <div>
-            <div className="inventory">
-                <span className="contents">
+            <h3>
+                Inventory
+            </h3>
                     <button className="redPotion" id="redPotion"></button>
                     <button className="bluePotion" id="bluePotion"></button>
                     <button className="greenPotion" id="greenPotion"></button>
                     <button className="yellowPotion" id="yellowPotion"></button>
-                </span>
-            </div>
-            "this is inventory"
         </div>
     )
 }
